c7b15af57816f0f8b3fc14a51e1d0cfee655d37f
JokinhasProgramassoes <joaotomaspereiraribeiro2004@gmail.com>
2026-04-27 19:02:22 +0100

    Commit dia 27/04/2026 - 19:01
    Realizei:
    Lesson 5.1 - Clicky Mouse

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+
+    public float spawnRate = 1.0f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+}
